4e0b2b4e76302987dbf613578a5400886a084b15
Alfon <MaschioAlfonso@Gmail.com>
2017-06-21 14:19:10 +0200

    docker feature

--- flutodo/settings.py
+++ flutodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vogd+ft91y)n=7)l2hrii@b1!t0hrr!go8u7k!@wa=58qv!hr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'flutodo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
