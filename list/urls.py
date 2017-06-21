e30592441e2533cfbdd882ea7d9efde188ffa569
Alfon <MaschioAlfonso@Gmail.com>
2017-06-21 12:35:28 +0200

    Initial commit

---
+++ list/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url, include
+from . import views
+from rest_framework import routers
+from django.contrib.auth.views import login, logout
+from list.views import TodoListView, TodoListCreate, TodoListDelete, toogle_todo,UserViewSet,GroupViewSet, TodoListViewSet
+from .serializers import UserSerializer, GroupSerializer, TodoListSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'todolist', TodoListViewSet)
+
+
+urlpatterns = [
+    url(r'^$', TodoListView.as_view(), name='index'),
+    url(r'create_todo', TodoListCreate.as_view(), name='create_todo'),
+    url(r'delete_todo/(?P<pk>\d+)/$', TodoListDelete.as_view(), name='delete_todo'),
+    url(r'toogle_todo/(?P<pk>[0-9]+)/$', toogle_todo, name='toogle_todo'),
+    url(r'create_user', views.create_user, name='create_user'),
+    url(r'^accounts/login/$', login,
+       {'template_name': 'admin/login.html'}),
+    url(r'^accounts/logout/$', logout,  {'next_page': '/list'}),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
---
+++ list/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
+from .models import TodoList
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, TodoListSerializer
+
+
+class TodoListView(ListView):
+    model = TodoList
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TodoListView, self).dispatch(request, *args, **kwargs)
+
+class TodoListCreate(CreateView):
+    model = TodoList
+    fields = ['name']
+    success_url = reverse_lazy('index')
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TodoListCreate, self).dispatch(request, *args, **kwargs)
+
+class TodoListDelete(DeleteView):
+    model = TodoList
+
+    def get_success_url(self, **kwargs):
+        return reverse('index') + "?just_delete_name=" + self.object.name
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TodoListDelete, self).dispatch(request, *args, **kwargs)
+
+def TodoListUpdate(UpdateView):
+    model = TodoList
+    fields = ['name']
+    template_name_suffix = '_update_form'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TodoListUpdate, self).dispatch(request, *args, **kwargs)
+
+@login_required
+@require_http_methods(["POST"])
+def toogle_todo(request, pk):
+    item = TodoList.objects.get(pk=pk)
+    item.completed = not item.completed
+    item.save()
+    return HttpResponseRedirect('/list')
+
+
+# This is really bad, the UserStory said
+# "must register with the system.". I suggest to implement
+# a Oauth, like auth0 Authtenticator.
+@require_http_methods(["GET", "POST"])
+def create_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = UserCreationForm()
+        return render(request, 'list/create_user.html', {'form': form})
+
+# Rest API
+#
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class TodoListViewSet(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
